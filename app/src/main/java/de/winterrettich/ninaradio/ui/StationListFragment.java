a6b2436d77d194a7bdba419aedfc81169f589218
Mike <m.klimek@tu-bs.de>
2015-05-16 01:04:47 +0200

    fix animation start for external event

--- app/src/main/java/de/winterrettich/ninaradio/ui/StationListFragment.java
+++ app/src/main/java/de/winterrettich/ninaradio/ui/StationListFragment.java
@@ -3,8 +3,8 @@ package de.winterrettich.ninaradio.ui;
 import android.app.Fragment;
 import android.content.Context;
 import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.Drawable;
 import android.graphics.drawable.StateListDrawable;
-import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -30,13 +30,12 @@ public class StationListFragment extends Fragment implements AdapterView.OnItemC
     private ListView mListView;
     private StationsListAdapter mAdapter;
     private LinkedList<Station> mStations = new LinkedList<>();
-    private AnimationDrawable mSelectedItemIconAnimation;
+    private PlaybackEvent.Type mPlaybackState = PlaybackEvent.Type.PLAY;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_station_list, container, false);
 
-        MediaPlayer mediaPlayer = new MediaPlayer();
         mStations.add(new Station("Rock", "http://197.189.206.172:8000/stream"));
         mStations.add(new Station("Blubb", "http://usa8-vn.mixstream.net:8138"));
 
@@ -53,48 +52,32 @@ public class StationListFragment extends Fragment implements AdapterView.OnItemC
 
     @Subscribe
     public void handlePlaybackEvent(PlaybackEvent event) {
-        switch (event.type) {
-            case PLAY:
-                break;
-            case PAUSE:
-                break;
-            case STOP:
-                mListView.clearChoices();
-                mListView.setAdapter(mAdapter);
-                break;
+        mPlaybackState = event.type;
+        if(event.type == PlaybackEvent.Type.STOP) {
+            mListView.clearChoices();
         }
+        mAdapter.notifyDataSetChanged();
     }
+
+
 
     @Subscribe
     public void handleSelectStationEvent(SelectStationEvent event) {
         if(mStations.contains(event.station)) {
             int position = mAdapter.getPosition(event.station);
             mListView.setItemChecked(position, true);
+            mAdapter.notifyDataSetChanged();
         }
     }
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-
-        // Animation
-        if(mSelectedItemIconAnimation != null) {
-            // cancel animation from previous item
-            mSelectedItemIconAnimation.stop();
-        }
-        ImageView icon = (ImageView) view.findViewById(R.id.icon);
-        StateListDrawable stateListDrawable = (StateListDrawable) icon.getDrawable();
-        mSelectedItemIconAnimation = (AnimationDrawable) stateListDrawable.getCurrent();
-        mSelectedItemIconAnimation.setVisible(true, true);
-        mSelectedItemIconAnimation.start();
-
         Station station = (Station) parent.getItemAtPosition(position);
-
         RadioApplication.sBus.post(new SelectStationEvent(station));
         RadioApplication.sBus.post(new PlaybackEvent(PlaybackEvent.Type.PLAY));
     }
 
-    private static class StationsListAdapter extends ArrayAdapter<Station> {
+    private class StationsListAdapter extends ArrayAdapter<Station> {
 
         public StationsListAdapter(Context context, List<Station> objects) {
             super(context, 0, objects);
@@ -108,12 +91,49 @@ public class StationListFragment extends Fragment implements AdapterView.OnItemC
                 convertView = LayoutInflater.from(getContext()).inflate(R.layout.station_list_item, parent, false);
             }
 
+            ImageView icon = (ImageView) convertView.findViewById(R.id.icon);
+            StateListDrawable stateListDrawable = (StateListDrawable) icon.getDrawable();
+
+            int checkedPosition = ((ListView) parent).getCheckedItemPosition();
+            if(position == checkedPosition && mPlaybackState == PlaybackEvent.Type.PLAY) {
+                startIconAnimation(convertView);
+            } else {
+                stopIconAnimation(convertView);
+            }
+
             TextView nameTextView = (TextView) convertView.findViewById(R.id.name);
             TextView descriptionTextView = (TextView) convertView.findViewById(R.id.description);
             nameTextView.setText(station.name);
             descriptionTextView.setText(station.url);
 
             return convertView;
+        }
+
+        private void startIconAnimation(View view) {
+            ImageView icon = (ImageView) view.findViewById(R.id.icon);
+            StateListDrawable stateListDrawable = (StateListDrawable) icon.getDrawable();
+            Drawable current = stateListDrawable.getCurrent();
+
+            if (current instanceof AnimationDrawable) {
+                AnimationDrawable animation = (AnimationDrawable) current;
+                if(!animation.isRunning()) {
+                    animation.setVisible(true, true);
+                    animation.start();
+                }
+            }
+        }
+
+        private void stopIconAnimation(View view) {
+            ImageView icon = (ImageView) view.findViewById(R.id.icon);
+            StateListDrawable stateListDrawable = (StateListDrawable) icon.getDrawable();
+            Drawable current = stateListDrawable.getCurrent();
+
+            if (current instanceof AnimationDrawable) {
+                AnimationDrawable animation = (AnimationDrawable) current;
+                if(animation.isRunning()) {
+                    animation.stop();
+                }
+            }
         }
 
     }
