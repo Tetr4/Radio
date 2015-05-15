107ee9e3719d5e68704687bf81fd70911f58ee59
Mike <m.klimek@tu-bs.de>
2015-05-15 19:12:06 +0200

    made parcelable to use as intent extra

--- app/src/main/java/de/winterrettich/ninaradio/model/Station.java
+++ app/src/main/java/de/winterrettich/ninaradio/model/Station.java
@@ -1,11 +1,9 @@
 package de.winterrettich.ninaradio.model;
 
-import java.net.URL;
+import android.os.Parcel;
+import android.os.Parcelable;
 
-/**
- * Created by mike on 08.05.15.
- */
-public class Station {
+public class Station implements Parcelable {
     public String name;
     public String url;
 
@@ -13,4 +11,33 @@ public class Station {
         this.name = name;
         this.url = url;
     }
+
+    public Station(Parcel in){
+        this.name = in.readString();
+        this.url = in.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeString(url);
+    }
+
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<Station> CREATOR = new Parcelable.Creator<Station>() {
+        @Override
+        public Station createFromParcel(Parcel in) {
+            return new Station(in);
+        }
+
+        @Override
+        public Station[] newArray(int size) {
+            return new Station[size];
+        }
+    };
 }
